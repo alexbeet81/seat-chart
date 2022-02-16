8c5f448caf631f11465fabf9000f11b899ca77d4
Alex Pritchard <alexbeet1234@gmail.com>
2022-02-16 18:31:33 +0800

    added a seat chart with editable grid

--- src/components/SeatChart.js
+++ src/components/SeatChart.js
@@ -1,57 +1,50 @@
-import GridLayout from "react-grid-layout";
-import { Responsive, WidthProvider } from "react-grid-layout";
-
+import { Fragment, useRef, useState } from "react";
+import StudentDesk from "./StudentDesk";
 import classes from "./SeatChart.module.css";
-import StudentCard from "./StudentCard";
-
-const ResponsiveGridLayout = WidthProvider(Responsive);
-
-const STUDNETS = [
-  {
-    id: "1",
-    number: "1",
-    name: "Brian",
-  },
-  {
-    id: "2",
-    number: "2",
-    name: "Sally",
-  },
-  {
-    id: "3",
-    number: "3",
-    name: "Dave",
-  },
-];
 
 const SeatChart = () => {
-  // const layout = [
-  //   { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-  //   { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-  //   { i: "c", x: 4, y: 0, w: 1, h: 2 },
-  // ];
+  const [rows, setRows] = useState()
+  const [columns, setColumns] = useState()
 
-  const layout = [
-    { i: "a", x: 10, y: 10, w: 1, h: 2, static: false },
-  ];
+  const rowsChangeHandler = (state) => {
+    setRows(state.target.value)
+  }
+
+  const columnsChangeHandler = (state) => {
+    setColumns(state.target.value)
+  }
 
   return (
-    <div className={classes.seatChart__Container}>
-      <ResponsiveGridLayout
-        className="layout"
-        layouts={layout}
-        breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-      >
-        {STUDNETS.map((student) => {
-          return (
-            <div key={student.id}>
-              <StudentCard name={student.name} number={student.number} />
-            </div>
-          );
-        })}
-      </ResponsiveGridLayout>
-    </div>
+    <Fragment>
+      <form>
+        <label htmlFor="rows">Rows</label>
+        <input onClick={rowsChangeHandler} id="z" type="number" />
+        <label htmlFor="columns">Columns</label>
+        <input onClick={columnsChangeHandler} id="x" type="number" />
+      </form>
+      <div className={classes.container}>
+        <div
+          className={classes.grid}
+          style={{
+            gridTemplateColumns: `repeat(${columns}, 1fr)`,
+            gridTemplateRows: `repeat(${rows}, 1fr)`,
+          }}
+        >
+          <StudentDesk />
+          <StudentDesk />
+          <StudentDesk />
+          <StudentDesk />
+          <StudentDesk />
+          <StudentDesk />
+          <StudentDesk />
+          <StudentDesk />
+          <StudentDesk />
+          <StudentDesk />
+          <StudentDesk />
+          <StudentDesk />
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
---
+++ src/components/StudentDesk.js
@@ -0,0 +1,7 @@
+import classes from './StudentDesk.module.css';
+
+const StudentDesk = () => {
+  return <div className={classes.desk}/>
+};
+
+export default StudentDesk;
