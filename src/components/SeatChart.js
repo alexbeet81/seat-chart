29b89ae274dad835bd3e026aa535bc6ff8024d73
Alex Pritchard <alexbeet1234@gmail.com>
2022-02-17 15:07:56 +0800

    tring to place desks into an array and return that array from the renderDesk function

--- src/components/SeatChart.js
+++ src/components/SeatChart.js
@@ -3,25 +3,90 @@ import StudentDesk from "./StudentDesk";
 import StudentCard from "./StudentCard";
 import classes from "./SeatChart.module.css";
 
-const renderDesk = (numberOfStudents, deskNumber) => {
-  // if numberOfStudents <= numberOfDesks
-  // render the desk with a student.
-  // else
-  // render the desk without a student.
-  // calculate remaing desks
-  const remainingStudents = numberOfStudents - deskNumber
+const DUMMY_STUDENTS = [
+  {
+    id: "s1",
+    studentNumber: "1",
+    name: "Burt",
+  },
+  {
+    id: "s2",
+    studentNumber: "2",
+    name: "Sarah",
+  },
+  {
+    id: "s3",
+    studentNumber: "3",
+    name: "Michael",
+  },
+  {
+    id: "s4",
+    studentNumber: "4",
+    name: "Jeff",
+  },
+  {
+    id: "s5",
+    studentNumber: "5",
+    name: "Sally",
+  },
+  {
+    id: "s6",
+    studentNumber: "6",
+    name: "Burt",
+  },
+  {
+    id: "s7",
+    studentNumber: "7",
+    name: "Sarah",
+  },
+  {
+    id: "s8",
+    studentNumber: "8",
+    name: "Michael",
+  },
+  {
+    id: "s9",
+    studentNumber: "9",
+    name: "Jeff",
+  },
+  {
+    id: "s10",
+    studentNumber: "10",
+    name: "Sally",
+  },
+];
 
-  if (remainingStudents >= 1) {
-    return <StudentDesk key={deskNumber}><StudentCard number={deskNumber + 1} name="name" /></StudentDesk>
-  } else {
-    return <StudentDesk key={deskNumber}>{null}</StudentDesk>
+const renderDesk = (deskNumber, students, columns) => {
+  const remainingStudents = students.length - deskNumber + 1;
+  // make an array equal to length of columns
+  let rowOfDesks = [];
+
+  console.log(columns, 'columns line 64');
+
+  for (let i = 0; i < columns; i++) {
+    console.log(i, "i on line 67");
+    if (students[deskNumber] !== undefined) {
+      let desk = (
+        <StudentDesk key={deskNumber}>
+          <StudentCard
+            number={students[deskNumber].studentNumber}
+            name={students[deskNumber].name}
+          />
+        </StudentDesk>
+      );
+      rowOfDesks.push(desk);
+    }
+    rowOfDesks.push(<StudentDesk key={deskNumber}>{null}</StudentDesk>);
   }
+
+  console.log(rowOfDesks);
+
+  return rowOfDesks;
 };
 
 const SeatChart = () => {
   const [rows, setRows] = useState(4);
   const [columns, setColumns] = useState(5);
-  const [numberOfStudents, setNumberOfStudents] = useState(10);
 
   const numberOfDesks = rows * columns;
 
@@ -33,10 +98,9 @@ const SeatChart = () => {
     setColumns(state.target.value);
   };
 
-  const totalDesks = [...Array(numberOfDesks)].map((element, index) => (
-    // <StudentDesk key={index}><StudentCard number={index + 1} name="name" /></StudentDesk>
-    renderDesk(numberOfStudents, index)
-  ));
+  const totalDesks = [...Array(numberOfDesks)].map((element, index) =>
+    renderDesk(index, DUMMY_STUDENTS, columns)
+  );
 
   return (
     <Fragment>
